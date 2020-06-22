9f7497a1b0a60b98dece43ff00b4c02869fd80a9
chainingless <chainingless@gmail.com>
2020-06-22 21:18:38 +0800

    feat: add definition of TrafficLight

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <chrono>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
@@ -25,8 +27,7 @@ void MessageQueue<T>::send(T &&msg)
 }
 
 /* Implementation of class "TrafficLight" */
-
-/* 
+ 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -34,9 +35,10 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+    while (true) {
+        TrafficLightPhase lightPhase = _messages.receive();
+        if (lightPhase == green) return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -46,16 +48,35 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
-{
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+{   
+    std::chrono::time_point<std::chrono::system_clock> lastUpdateTime = std::chrono::system_clock::now();
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        
+        long duriationSinceLastUpdate = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - lastUpdateTime).count();
+
+        std::default_random_engine generator;
+        std::uniform_int_distribution<int> distribution(4, 6);
+        int maxDuriation = distribution(generator);
+        
+        if (duriationSinceLastUpdate >= maxDuriation) {
+            switch (_currentPhase) {
+                case green:
+                    _currentPhase = red;
+                    break;
+                case red:
+                    _currentPhase = green;
+                    break;
+            }
+            _messages.send(std::move(_currentPhase));
+            lastUpdateTime = std::chrono::system_clock::now();
+        }
+    }
 }
 
-*/
